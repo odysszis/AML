afec8d82a8df13808bd10c6200e8e87a613c20ae
PeadarOhAodha <ucabpnh@ucl.ac.uk>
2016-02-01 02:39:28 +0000

    Load data update
    added methods for mapping target labels and storing loaded data to
    bumpy files for easier re-use

--- DataScienceBowl/LoadData.py
+++ DataScienceBowl/LoadData.py
@@ -2,28 +2,32 @@ from __future__ import print_function
 import os
 import numpy as np
 import dicom
-from scipy.misc import imresize
+from scipy import misc
 
 
 # TODO use pickle for object serialisation
-# TODO reshape data to collapse into array of pixels
+# TODO reshape data to collapse into array of pixels (TBC depending on how input must look for model
 # TODO add method for storing resulting numpy arrays as theano shared variables
-# TODO add method for mapping input images to ground truth masks
-# TODO include meta data within download
-
-# LOCAL TEST: data = load_dcm_data('/Users/Peadar/Documents/KagglePythonProjects/AML/data/validate', crop_resize, newsize = (64,48))
+# TODO add method for expanding training data set, as described in 'DSBC Challenge - Model outline'
 
 
 def load_dcm_data(directory, preprocess, **args):
 
     """
+    loads data from DCM files
+
     :param directory: top level folder where image .dcm files are stored
     :param preprocess: function handle which preprocessing method to apply to images
     :param **args: any named arguments and their values required by preprocess method
-    """
-    print('Loading from {0}...'.format(directory))
 
-    imagedata = [] #placeholder for imagedata array
+    RETURNS: imagedata: is a numpy array numimages * imageheight * imagewidth for processed images
+             imageids: identifier code for image containing info on study id
+    """
+
+    print('Source directory {0}...'.format(directory))
+
+    imagedata = []
+    imageids= []
 
     for root, _, files in os.walk(directory):
         total = 1
@@ -37,23 +41,27 @@ def load_dcm_data(directory, preprocess, **args):
             current_image = dicom.read_file(image_path)
             current_image = current_image.pixel_array.astype(float)/np.max(current_image.pixel_array) #between 0 and 1
             current_image = preprocess(current_image, **args) #applies selected prepocess routine
+            imageid = "%s-%s" % (f.rsplit('-')[1], f.rsplit('-')[2][0:4])
+            print('Image id loaded... {0}'.format(imageid))
 
             imagedata.append(current_image)
+            imageids.append(imageid)
 
-            print('Images loaded {0}'.format(total))
             total += 1
 
     imagedata = np.array(imagedata)
 
     print('Number of images {0}'.format(imagedata.shape[0]))
 
-    return imagedata
+    return imagedata, imageids
 
 
 
 def crop_resize(image, crop=False, newsize=()):
 
     """
+    Crops (optional) and resizes images
+
     :param image: numpy array to be processed
     :param crop: true/false whether to crop image from centre
     :param newsize: optional tuples for new image size
@@ -66,9 +74,41 @@ def crop_resize(image, crop=False, newsize=()):
         x = int((image.shape[1] - edge) / 2)
         image = image[y: y + edge, x: x + edge]
 
-    image = imresize(image, newsize)  #using scipy resize function tuple for new size
+    image = misc.imresize(image, newsize)  #using scipy resize function with tuple for new size
 
     return image
+
+
+
+def get_vol_labels(filename):
+
+    """
+    creates mapping between training images and their diastole and systole
+    volumes given in 'train.csv'. These act as targets in supervised learning problem.
+
+    :param filename: name of csv file containing the targets
+    """
+    file = open(filename)
+    targets= file.readlines()
+    targetmapping = []
+
+    i = 0
+    for item in targets:
+        if i > 0:
+            id, diastole, systole = item.replace('\n', '').split(',')
+            targetmapping.append([float(id), float(diastole), float(systole)])
+        i = i + 1
+
+    return targetmapping
+
+# Load data and store to numpy files for re-use
+
+data, ids = load_dcm_data('AML/data/train', crop_resize, newsize = (64,48))
+target = get_vol_labels('AML/data/train.csv')
+
+# numpy files will appear in data folder of directory
+np.save('AML/data/trainIn.npy', data)
+np.save('AML/data/trainOut.npy', target)
 
 
 
