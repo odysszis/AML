2d6933e50493931dd411bac078c82c13813ac829
PeadarOhAodha <ucabpnh@ucl.ac.uk>
2016-02-02 12:50:23 +0000

    Load Data for sunny brook update
    Overcame issue with contour and image ID mismatch by looking at dictionary mapping provided by kaggle caffe tutorial, where they have carried out the manual inspection process described by sunny brooks read me files.

--- DataScienceBowl/LoadDataSB.py
+++ DataScienceBowl/LoadDataSB.py
@@ -4,13 +4,13 @@ import fnmatch
 import re
 import numpy as np
 import dicom
-#import cv2
+import opencv_engine as oe
+import cv2
+from LoadData import crop_resize
 from scipy import misc
 
 
 # TODO add preprocessing on image data, like for kaggle training data LoadData routine
-# TODO it has become apparant that the contour IDs do not map directly image IDs! Must fix code to deal with this
-
 
 # Declare the top level directories that hold the image and contour files within the sunnybrook data
 
@@ -48,12 +48,16 @@ def get_mapping(contour_dir):
             lookup = re.search(r"/([^/]*)/contours-manual/IRCCI-expert/IM-(\d{4})-(\d{4})-icontour-manual.txt", path)
 
             c_series.append(lookup.group(1)) # Contour series ID
-            c_imgid.append("%s-%s" % (lookup.group(2), lookup.group(3)))  # Contour corresponding image ID based on parsed regular expression
+            #c_imgid.append("%s-%s" % (lookup.group(2), lookup.group(3)))  # Contour corresponding image ID based on parsed regular expression
+            c_imgid.append(lookup.group(3))
+
+    print("Number of contours: {:d}".format(len(c_path)))
 
     return c_path, c_series, c_imgid
 
 
-def load_contours_dcm(c_path, c_series, c_imgid, image_dir):
+def load_contours_dcm(c_path, c_series, c_imgid,
+                      image_dir, training_dic, preprocess, **args):
 
     """
     loads data from DCM files into numpy arrays, and creates mask given contours
@@ -70,15 +74,20 @@ def load_contours_dcm(c_path, c_series, c_imgid, image_dir):
 
     for i in range(0, len(c_series)):
 
-        file = "IM-%s.dcm" % (c_imgid[i])  #builds image path based on ID returned from contour
+        file = "IM-%s-%s.dcm" % (training_dic[c_series[i]], c_imgid[i])  #From caffe tutorial; builds image path based on ID returned from contour
         path = os.path.join(image_dir, c_series[i], file)
         current_image = dicom.read_file(path)
-        current_image = current_image.pixel_array.astype(np.int)
+        current_image = current_image.pixel_array.astype(float)/np.max(current_image.pixel_array) #between 0 and 1
         contour = np.loadtxt(c_path[i], delimiter=" ").astype(np.int)
         mask = np.zeros_like(current_image, dtype="uint8")
-        mask[contour] = 1
-        ##cv2.fillPoly(mask, [contour], 1) #Need to install dependencies for cv2
+        #mask[contour] = 1
+        cv2.fillPoly(mask, [contour], 1) #Need to install dependencies for cv2
 
+        #further preprocess image by passed method preprocess
+        current_image = preprocess(current_image, **args)
+        mask = preprocess(mask, **args)
+
+        #collect data
         imagedata.append(current_image)
         contourdata.append(mask)
 
@@ -91,14 +100,46 @@ def load_contours_dcm(c_path, c_series, c_imgid, image_dir):
     return imagedata, contourdata
 
 
+# There is a manual process to map contours to images, as IDs dont match exactly. Resulting in Dic:
+
+SAX_SERIES = {
+
+    "SC-HF-I-01": "0004",
+    "SC-HF-I-02": "0106",
+    "SC-HF-I-04": "0116",
+    "SC-HF-I-40": "0134",
+    "SC-HF-NI-03": "0379",
+    "SC-HF-NI-04": "0501",
+    "SC-HF-NI-34": "0446",
+    "SC-HF-NI-36": "0474",
+    "SC-HYP-01": "0550",
+    "SC-HYP-03": "0650",
+    "SC-HYP-38": "0734",
+    "SC-HYP-40": "0755",
+    "SC-N-02": "0898",
+    "SC-N-03": "0915",
+    "SC-N-40": "0944",
+}
+
+
 # Load data and store to numpy files for re-use
 
 c_path, c_series, c_imgid = get_mapping(contour_dir)
-imagedata, contourdata = load_contours_dcm(c_path, c_series, c_imgid, image_dir)
+imagedata, contourdata = load_contours_dcm(c_path, c_series, c_imgid,
+                                           image_dir, SAX_SERIES, crop_resize, newsize = (64,48))
 
 # numpy files will appear in data folder of directory
 
 np.save('AML/data/SBtrainImage.npy', imagedata)
 np.save('AML/data/SBtrainMask.npy', contourdata)
+
+
+
+
+
+
+
+
+
 
 
