d5e3219237d4972aa5a48f115e5969dad65c4bb2
Hursh Prasad <hursh.prasad.14@ucl.ac.uk>
2016-02-11 16:37:45 +0000

    fixed more syntac for logging, must set epsilon for grid search

--- DataScienceBowl/convolution/LeNet.py
+++ DataScienceBowl/convolution/LeNet.py
@@ -18,7 +18,6 @@ from logisticReg import LogisticRegression, load_data
 
 #matplotlib.use('Agg')
 
-logging.basicConfig(filename='lenet.log', filemode='w', level=logging.INFO)
 
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
@@ -264,20 +263,20 @@ def fine_tuning(learning_rate = 0.1, n_epochs = 1000, nkerns = 100, batch_size =
     print('... fine tuning')
 
     epoch = 0
-    epsilon = 0.00005
+    epsilon = 0.0000005
     last_loss = 0
 
+    logging.debug('%-10s%-10s%-10s' %('Epoch','Batch','Cost'))
     while (epoch < n_epochs):
         epoch += 1
-        logging.debug('%-10Epoch%-10batch%-10cost')
         for minibatch_index in xrange(n_train_batches):
 
             cost_ij = train_model(minibatch_index)
             #print '\nepoch = %s' % epoch
             #print 'batch = %s' % minibatch_index
-            #print 'cost = %s' % cost_ij
+            print 'cost = %s' % cost_ij
             logging.debug('%-10s %-10s %-10s' % (epoch, minibatch_index, cost_ij))
-            if not cost_ij - last_loss >= epsilon:
+            if cost_ij - last_loss <= epsilon:
                 #print 'converged: %.2f' % (cost_ij - last_loss)
                 logging.debug('Converged %s'%(cost_ij - last_loss))
                 return
@@ -363,6 +362,7 @@ def predict(nkerns = 100, batch_size = 260, fine_tuned_params_path = None):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='lenet.log', filemode='w', level=logging.DEBUG)
     # call the following method for fine tuning after pre-training the logistic regression layer
     fine_tuning(n_epochs=1000,batch_size=20,logistic_params_path = '../data/logistic_params.pickle',CNN_inputFilters_path = '../data/CNN_inputFilters',CNN_inputBias_path = '../data/CNN_inputBias')
     # call this method to predict outcome after fine tuning
